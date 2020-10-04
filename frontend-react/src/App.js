95ef3965e1d35f12ba4a56b34fbbd8c9843781d2
Upshur06 <mupshur2005@gmail.com>
2020-10-03 23:50:49 -0400

    mark up

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState("");
+  const [percent, setPercent] = useState("");
+  const [tip, setTip] = useState("Tip Goes Here");
   return (
     <div className="App">
-        {/* amount */}
-        
-        {/* percentage */}
-        {/* output */}
+      {/* amount */}
+      <input
+        type="text"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      {/* percentage */}
+      <input
+        type="text"
+        placeholder="Percent"
+        value={percent}
+        onChange={(e) => setPercent(e.target.value)}
+      />
+
+      <h1>{tip}</h1>
+      {/* output */}
     </div>
   );
 }
