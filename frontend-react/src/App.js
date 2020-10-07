edcb4bddd2981eb05141034821c87205fad1f984
Upshur06 <mupshur2005@gmail.com>
2020-10-07 00:00:05 -0400

    equal

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -6,25 +6,52 @@ function App() {
   const [amount, setAmount] = useState("");
   const [percent, setPercent] = useState("");
   const [tip, setTip] = useState("Tip Goes Here");
+
+  const calculateTip = (e) => {
+    e.preventDefault();
+
+    const data = {
+      amount: amount,
+      tip: percent,
+    };
+
+    fetch("http://localhost:9000/api/v1/calculatetip", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setTip(data.toBePayed);
+      });
+  };
+
   return (
     <div className="App">
       {/* amount */}
-      <input
-        type="text"
-        placeholder="Amount"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
-      {/* percentage */}
-      <input
-        type="text"
-        placeholder="Percent"
-        value={percent}
-        onChange={(e) => setPercent(e.target.value)}
-      />
+      <form>
+        <input
+          type="text"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        {/* percentage */}
+        <input
+          type="text"
+          placeholder="Percent"
+          value={percent}
+          onChange={(e) => setPercent(e.target.value)}
+        />
 
-      <h1>{tip}</h1>
-      {/* output */}
+        <button onClick={calculateTip}>Send</button>
+        <h1>{tip}</h1>
+        {/* output */}
+      </form>
     </div>
   );
 }
--- node_backend/server.js
+++ node_backend/server.js
@@ -1,6 +1,7 @@
 // import dependencies
 
 const express = require("express");
+const cors = require("cors");
 
 // app config
 const app = express();
@@ -8,10 +9,20 @@ const port = 9000;
 
 // middlewares
 app.use(express.json());
+app.use(cors());
 
 //api routes
 app.get("/", (req, res) => {
-  res.status(200).send("hello world");
+  res.status(200).json("Hello World");
+});
+
+app.post("/api/v1/calculatetip", (req, res) => {
+  const amount = parseInt(req.body.amount);
+  const tip = parseInt(req.body.tip);
+
+  const toBePayed = amount + (tip / 100) * amount;
+
+  res.status(200).json({ toBePayed });
 });
 
 // listen
