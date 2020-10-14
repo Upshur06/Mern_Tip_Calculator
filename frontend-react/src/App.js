b94ecadf03255a54d02809d356a37e3441c258cc
Upshur06 <mupshur2005@gmail.com>
2020-10-14 12:04:43 -0400

    center input form

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -33,36 +33,45 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Trial</h1>
-      
+        <h1 id="header">Tip Calculator</h1>
 
-      <form>
-      <div className="form-row">
-        <div className="form-group col-md-6">
-          <label>Amount</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Amount"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
+        <form>
+          <div className="form-row">
+            <div className="form-group form-control-lg col-lg-7">
+              <label>Amount</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group form-control-lg col-lg-7">
+              <label>Percent</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Percent"
+                value={percent}
+                onChange={(e) => setPercent(e.target.value)}
+              />
+            </div>
+          </div>
+        </form>
+        <div className="button-container">
+          <button
+            type="button"
+            class="btn btn-secondary btn-lg btn-block"
+            onClick={calculateTip}
+          >
+            <h2>Calculate</h2>
+          </button>
         </div>
-        <div className="form-group col-md-6">
-          <label>Percent</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Percent"
-            value={percent}
-            onChange={(e) => setPercent(e.target.value)}
-          />
+        <div className="jumbotron">
+          <h1>{tip}</h1>
         </div>
-      </div>    
-        <button className="btn btn-primary btn-lg btn-block" onClick={calculateTip}>Send</button>
-        <h1>{tip}</h1>
-        
-      </form>
       </div>
     </div>
   );
